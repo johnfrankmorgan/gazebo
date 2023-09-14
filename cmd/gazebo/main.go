57e25fbc155da0d2d9d3a87ce11faf7fbd7531f3
johnfrankmorgan <johnfrankmorgan@gmail.com>
2023-09-14 21:28:20 +0100

    add --profile flag

--- cmd/gazebo/main.go
+++ cmd/gazebo/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"gazebo/util/must"
+	"os"
+	"runtime/pprof"
+
 	"github.com/alecthomas/kong"
 )
 
@@ -9,9 +14,23 @@ func main() {
 		Run     Run     `cmd:"" default:"withargs" help:"Executes the specified file."`
 		Parse   Parse   `cmd:"" help:"Parses the specified file(s) and prints the resulting AST(s)."`
 		Compile Compile `cmd:"" help:"Compiles the specified file(s) and prints the resulting bytecode."`
+		Profile string  `help:"Write profiling information here."`
 	}{}
 
 	ctx := kong.Parse(&cli)
+
+	if cli.Profile != "" {
+		f, err := os.Create(cli.Profile)
+		ctx.FatalIfErrorf(err)
+
+		defer must.Close(f)
+
+		ctx.FatalIfErrorf(pprof.StartCPUProfile(f))
+
+		defer pprof.StopCPUProfile()
+
+		fmt.Println("writing profiling information to", f.Name())
+	}
 
 	ctx.FatalIfErrorf(ctx.Run())
 }
--- util/must/must.go
+++ util/must/must.go
@@ -1,11 +1,22 @@
 package must
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+)
 
-func Succeed[T any](value T, err error) T {
+func NotError(err error) {
 	if err != nil {
 		panic(fmt.Errorf("must: unexpected error: %w", err))
 	}
+}
+
+func Succeed[T any](value T, err error) T {
+	NotError(err)
 
 	return value
+}
+
+func Close(c io.Closer) {
+	NotError(c.Close())
 }
