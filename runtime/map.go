467fd2b7503d76756169a4e6a45c63e7906e2d32
Frank Morgan <johnfrankmorgan@gmail.com>
2024-06-09 17:10:32 +0100

    add missing Map methods (#23)

--- runtime/map.go
+++ runtime/map.go
@@ -29,14 +29,6 @@ func (m *Map) Get(key Object) (Object, Bool) {
 	return m.ht.get(key)
 }
 
-func (m *Map) GetIndex(index Object) Object {
-	if value, ok := m.Get(index); ok {
-		return value
-	}
-
-	panic(Exc.NewKeyNotFound(index))
-}
-
 func (m *Map) Set(key, value Object) {
 	m.ht.set(key, value)
 }
